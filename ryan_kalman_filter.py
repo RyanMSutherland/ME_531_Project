c6fad05b80db83bf6f4e4e20a365055aea5830d5
RyanMSutherland <Ryan.Suth01@gmail.com>
2025-05-02 02:09:26 +0000

    Save

--- ryan_kalman_filter.py
+++ ryan_kalman_filter.py
@@ -31,15 +31,18 @@ class FlexToFListener(Node):
         self.get_logger().info('FlexToFListener node has been started.')
 
         # Kalman Filter Initialization
-        n = 2 # Number of outputs/state variables
+        n = 4 # Number of outputs/state variables
         m = 4 # Number of input measurements
         self.z = np.zeros((m, 1)) #[x, y] - measurements
         self.x = np.zeros((n, 1)) # Output estimate of state variables
-        self.R = np.ones((m, m)) # Measurement covariance matrix - Input
+        #self.R = np.ones((m, m)) # Measurement covariance matrix - Input
+        self.R = np.eye(m)
         self.P = np.ones((n, n)) # Estimate covariance matrix
-        self.H = np.ones((m, n)) # State to measurement matrix - System Model
+        #self.H = np.ones((m, n)) # State to measurement matrix - System Model
+        self.H = np.eye(n)
         self.A = np.ones((n, n)) # State transition matrix - system model
-        self.Q = np.ones((n, n)) # Process noise covariance matrix - system model
+        #self.Q = np.ones((n, n)) # Process noise covariance matrix - system model
+        self.Q = np.eye(n)
 
     def flex_callback(self, msg: Float32MultiArray):
         values = list(msg.data)  # Convert array('f', [...]) to a regular Python list
@@ -51,25 +54,27 @@ class FlexToFListener(Node):
         # Perform the Kalman Filter update
         self.kalman_update(measurement)
 
+        print(f'Predicted values: {self.x[0]}, {self.x[1]}, {self.x[2]}, {self.x[3]}')
+
         # Use the state estimate (position) to determine the apple's position
-        apple_position_x = self.x[0]
-        apple_position_y = self.x[1]
+        # apple_position_x = self.x[0]
+        # apple_position_y = self.x[1]
 
-        if apple_position_y > 1.0:
-            vertical_position = "Up"
-        elif apple_position_y < -1.0:
-            vertical_position = "Down"
-        else:
-            vertical_position = ""
+        # if apple_position_y > 1.0:
+        #     vertical_position = "Up"
+        # elif apple_position_y < -1.0:
+        #     vertical_position = "Down"
+        # else:
+        #     vertical_position = ""
 
-        if apple_position_x > 1.0:
-            horizontal_position = "Right"
-        elif apple_position_x < -1.0:
-            horizontal_position = "Left"
-        else:
-            horizontal_position = ""
+        # if apple_position_x > 1.0:
+        #     horizontal_position = "Right"
+        # elif apple_position_x < -1.0:
+        #     horizontal_position = "Left"
+        # else:
+        #     horizontal_position = ""
         
-        print(f"Apple Position: {vertical_position} {horizontal_position} ({apple_position_x:.2f}, {apple_position_y:.2f})")
+        # print(f"Apple Position: {vertical_position} {horizontal_position} ({apple_position_x:.2f}, {apple_position_y:.2f})")
 
     def tof_callback(self, msg: Int32):
         # print(f"[ToF Sensor Data] Distance: {msg.data} mm")
@@ -80,14 +85,16 @@ class FlexToFListener(Node):
         # Need to check what measurements look like, ensure as expected
         self.z = measurement
         # Initial update and covariance
-        self.x_p = np.matmul(self.A, self.x)
-        self.P_p = np.matmul(np.matmul(self.A, self.P), self.A.transpose()) + self.Q
+        self.x_p = np.dot(self.A, self.x)
+        self.P_p = np.dot(np.dot(self.A, self.P), self.A.transpose()) + self.Q
 
         # Compute Kalman Gain
-        self.K = np.matmul(np.matmul(self.P, self.H.transpose()), np.linalg.inv(np.matmul(np.matmul(self.H, self.P), self.H.transpose()) + self.R))
+        self.K = np.dot(np.dot(self.P, self.H.transpose()), np.linalg.inv(np.dot(np.dot(self.H, self.P), self.H.transpose()) + self.R))
         
-        self.x = self.x_p + np.matmul(self.K, (self.z - np.matmul(self.H, self.x_p)))
-        self.P = self.P_p - np.matmul(np.matmul(self.K, self.H), self.P_p)
+        self.x = self.x_p + np.dot(self.K, (self.z - np.dot(self.H, self.x_p)))
+        self.P = self.P_p - np.dot(np.dot(self.K, self.H), self.P_p)
+
+        print(f'x_p - {self.x_p}')
 
 
 def main():
